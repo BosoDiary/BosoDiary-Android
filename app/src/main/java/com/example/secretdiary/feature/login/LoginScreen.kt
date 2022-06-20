4d38c860a1f678b09fd90d1d0350868c79ecfbee
ckrudals <2004ckck@naver.com>
2022-06-20 17:47:38 +0900

    feat : Add login screen

--- app/src/main/java/com/example/secretdiary/feature/login/LoginScreen.kt
+++ app/src/main/java/com/example/secretdiary/feature/login/LoginScreen.kt
@@ -4,15 +4,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Icon
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,6 +29,7 @@ import com.example.secretdiary.ui.theme.mySoulFont
 fun LoginScreen(navController: NavController) {
 
     Column(
+
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
@@ -45,7 +49,7 @@ fun LoginTitle() {
 
 
     Box(
-            modifier = Modifier
+        modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight(align = Alignment.CenterVertically)
             .padding(start = 48.dp, end = 48.dp),
@@ -62,38 +66,53 @@ fun LoginTitle() {
 
 @Composable
 fun GoogleLoginButton(onclick: () -> Unit) {
+
     Box(
         modifier = Modifier
-            .background(loginBtnColor)
+            .padding(30.dp)
             .fillMaxWidth()
+            .background(loginBtnColor)
             .height(50.dp)
-            .padding(start = 30.dp, end = 30.dp)
             .clickable { onclick() }
     ) {
 
 
-        Row() {
-
-            Card(
-                contentColor = Color.White,
+        Row {
+            GoogleImageView()
+            Text(
+                text = "LOG IN WITH GOOGLE",
+                color = Color.White,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.ExtraBold,
                 modifier = Modifier
-                    .fillMaxHeight()
-                    .width(36.dp)
-                    .align(alignment = Alignment.CenterVertically)
-                    .padding(7.dp),
-                shape = RoundedCornerShape(8.dp)
-            ) {
-                Icon(
-                    modifier = Modifier.wrapContentSize(),
-                    painter = painterResource(id = R.drawable.google_img),
-                    contentDescription = "icon",
-
-                    )
-            }
+                    .align(Alignment.CenterVertically)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
         }
 
     }
 
+}
+
+@Composable
+fun GoogleImageView() {
+    Surface(
+        modifier = Modifier
+            .padding(7.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .size(36.dp)
+            .background(Color.White)
+
+    ) {
+        Icon(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(7.dp),
+            painter = painterResource(id = R.drawable.google_img),
+            contentDescription = null,
+        )
+    }
 }
 
 @Preview
