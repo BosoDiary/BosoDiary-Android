9384655891554339707fc6e4916d99559041dca3
ckrudals <2004ckck@naver.com>
2022-06-21 08:56:24 +0900

    chore : uncomment requestIdToken

--- app/src/main/java/com/example/secretdiary/feature/login/getGoogleSignInClient.kt
+++ app/src/main/java/com/example/secretdiary/feature/login/getGoogleSignInClient.kt
@@ -8,7 +8,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 fun getGoogleSignInClient(context: Context): GoogleSignInClient {
     val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        //.requestIdToken(context.getString(R.string.default_web_client_id ))
+        .requestIdToken(context.getString(R.string.default_web_client_id ))
         .requestEmail()
         .build()
 
